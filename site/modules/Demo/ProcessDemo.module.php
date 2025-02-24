d437046eb18412081f5663dd48e0f73466be9319
BernhardBaumrock <office@baumrock.com>
2025-02-24 16:59:37 +0100

    add and install demo module

---
+++ site/modules/Demo/ProcessDemo.module.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ProcessWire;
+
+class ProcessDemo extends Process
+{
+  public static function getModuleInfo()
+  {
+    return [
+      'version' => '1.0.0',
+      'requires' => [],
+      'installs' => [],
+      'page' => [
+        'name' => 'demo',
+        'parent' => 2,
+        'title' => 'JS Hooks Demo',
+      ],
+    ];
+  }
+
+  public function init()
+  {
+    parent::init(); // always remember to call the parent init
+  }
+
+  public function execute()
+  {
+    return wire()->files->render(__DIR__ . '/demo.php');
+  }
+}
